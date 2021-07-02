dd5643272ceff13232522094598f7d13a3dc0cc1
Renato Groffe <renato.groffe@yahoo.com.br>
2021-07-02 18:40:51 -0300

    .NET 5 + ASP.NET Core + REST API Contagem de Acessos + MassTransit + Apache Kafka
    .NET 5 + ASP.NET Core + REST API Contagem de Acessos + MassTransit + Apache Kafka

---
+++ APIContagem/Controllers/ContadorController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using APIContagem.Models;
+using MassTransit.KafkaIntegration;
+
+namespace APIContagem.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ContadorController : ControllerBase
+    {
+        private static readonly Contador _CONTADOR = new Contador();
+        private readonly ILogger<ContadorController> _logger;
+        private readonly IConfiguration _configuration;
+        private readonly ITopicProducer<IResultadoContador> _producer;
+
+        public ContadorController(ILogger<ContadorController> logger,
+            IConfiguration configuration,
+            ITopicProducer<IResultadoContador> producer)
+        {
+            _logger = logger;
+            _configuration = configuration;
+            _producer = producer;
+        }
+
+        [HttpGet]
+        public ResultadoContador Get()
+        {
+            int valorAtualContador;
+            lock (_CONTADOR)
+            {
+                _CONTADOR.Incrementar();
+                valorAtualContador = _CONTADOR.ValorAtual;
+            }
+
+            _logger.LogInformation($"Contador - Valor atual: {valorAtualContador}");
+
+            var resultado = new ResultadoContador()
+            {
+                ValorAtual = valorAtualContador,
+                Producer = _CONTADOR.Local,
+                Kernel = _CONTADOR.Kernel,
+                TargetFramework = _CONTADOR.TargetFramework,
+                Mensagem = _configuration["MensagemVariavel"]
+            };
+
+            _producer.Produce(resultado);
+            
+            return resultado;
+        }
+    }
+}
---
+++ APIContagem/Startup.cs
@@ -0,0 +1,78 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+using MassTransit;
+using MassTransit.KafkaIntegration;
+using APIContagem.Models;
+
+namespace APIContagem
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "APIContagem", Version = "v1" });
+            });
+
+            
+            services.AddMassTransit(bus =>
+            {
+                bus.UsingInMemory((context,cfg) => cfg.ConfigureEndpoints(context));
+
+                bus.AddRider(rider =>
+                {
+                    rider.AddProducer<IResultadoContador>("topic-testes");
+
+                    rider.UsingKafka(
+                        new ()
+                        {
+                            SecurityProtocol = Confluent.Kafka.SecurityProtocol.SaslSsl,
+                            SaslMechanism = Confluent.Kafka.SaslMechanism.Plain,
+                            SaslUsername = Configuration["ApacheKafka:Username"],
+                            SaslPassword = Configuration["ApacheKafka:Password"]
+                        },
+                        (_, kafka) =>
+                        {
+                            kafka.Host(Configuration["ApacheKafka:Host"]);
+                        });
+                });
+            });
+
+            services.AddMassTransitHostedService();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+                app.UseDeveloperExceptionPage();
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "APIContagem v1"));
+
+            app.UseCors(builder => builder.AllowAnyMethod()
+                                          .AllowAnyOrigin()
+                                          .AllowAnyHeader());
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
